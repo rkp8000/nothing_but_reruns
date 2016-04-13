7387a56e3ce8e9350229e33a9c75cca632395597
rkp8000 <rpang.contact@gmail.com>
2016-04-13 16:21:45 -0700

    Add set_fontsize function.

--- plot.py
+++ plot.py
@@ -2,6 +2,19 @@ from __future__ import division, print_function
 import numpy as np
 
 
+def set_fontsize(ax, fontsize):
+    """Set fontsize of all axis text objects to specified value."""
+
+    for txt in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                ax.get_xticklabels() + ax.get_yticklabels()):
+        txt.set_fontsize(fontsize)
+
+    legend = ax.get_legend()
+    if legend:
+        for txt in legend.get_texts():
+            txt.set_fontsize(fontsize)
+
+
 def fancy_raster(ax, spikes, drives, spike_marker_size=10, drive_marker_base_size=100):
     """
     Plot raster plots where each spike is a small dark solid circle and each drive is a surrounding hollow brighter
