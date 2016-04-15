0745320806cc3cfae79f96d2844626dc90ab98af
rkp8000 <rpang.contact@gmail.com>
2016-04-15 11:53:37 -0700

    Add demo to illustrate nonreplayable sequences.

--- figures/__init__.py
+++ figures/__init__.py
@@ -3,3 +3,4 @@ warnings.filterwarnings('ignore')
 
 from feed_forward_demos import replay_example as feed_forward_replay_example
 from feed_forward_demos import replay_probability_calculation as feed_forward_replay_probability_calculation
+from feed_forward_demos import nonreplayable_sequence_example as feed_forward_nonreplayable_sequence_example
--- figures/feed_forward_demos.py
+++ figures/feed_forward_demos.py
@@ -14,6 +14,7 @@ def make_feed_forward_grid_weights(shape, spread):
 
     return w
 
+
 def replay_example(
         SEED, GRID_SHAPE, LATERAL_SPREAD, G_W, G_X, G_D, T_X,
         DRIVEN_NODES, DRIVE_AMPLITUDE,
@@ -29,7 +30,7 @@ def replay_example(
     w = make_feed_forward_grid_weights(GRID_SHAPE, LATERAL_SPREAD)
     ntwk = network(w=w, g_w=G_W, g_x=G_X, g_d=G_D, t_x=T_X)
 
-    # make drive sequence
+    # loop over trials
 
     all_drives = []
     all_rs = []
@@ -39,6 +40,8 @@ def replay_example(
         run_time = 2 * len(driven_nodes_one_trial)
         n_nodes = w.shape[0]
 
+        # make drive sequence
+
         drives = np.zeros((run_time, n_nodes), dtype=float)
 
         for t, node in enumerate(driven_nodes_one_trial):
@@ -47,6 +50,7 @@ def replay_example(
         drives[len(driven_nodes_one_trial), driven_nodes_one_trial[0]] = DRIVE_AMPLITUDE
 
         # run network
+
         r_0 = np.zeros((n_nodes,))
         xc_0 = np.zeros((n_nodes,))
         rs = ntwk.run(r_0=r_0, drives=drives, xc_0=xc_0)
@@ -62,18 +66,20 @@ def replay_example(
         sharex=True, sharey=True, tight_layout=True)
 
     if n_trials == 1:
+
         axs = [axs]
 
     for ax, drives, rs in zip(axs, all_drives, all_rs):
+
         fancy_raster(ax, rs, drives)
 
     for ax_ctr, ax in enumerate(axs):
 
         ax.set_ylim(-1, n_nodes)
-        ax.set_ylabel('Ensemble')
+        ax.set_ylabel('ensemble')
 
         if ax_ctr == n_trials - 1:
-            ax.set_xlabel('Time step')
+            ax.set_xlabel('time step')
 
         set_fontsize(ax, FONT_SIZE)
 
@@ -113,7 +119,6 @@ def replay_probability_calculation(
         xc_0[test_sequence] = np.arange(T_X - run_time, T_X) + 1
 
         drives = np.zeros((run_time, n_nodes))
-
         drives[0, driven_node] = DRIVE_AMPLITUDE
 
         # loop through all "fixed parameter sets"
@@ -178,5 +183,103 @@ def replay_probability_calculation(
             ax.set_xlabel('gbar')
 
         ax.set_ylabel('replay \n probability')
+
+        set_fontsize(ax, FONT_SIZE)
+
+
+def nonreplayable_sequence_example(
+        SEED, GRID_SHAPE, LATERAL_SPREAD,
+        G_BAR_EXAMPLE, G_BARS, G_W_STAR, G_X_STAR, G_D_STAR, T_X,
+        DRIVEN_NODES, DRIVE_AMPLITUDE,
+        FIG_SIZE, FONT_SIZE):
+
+    # RUN SIMULATION
+
+    np.random.seed(SEED)
+
+    # make weight matrix
+
+    w = make_feed_forward_grid_weights(GRID_SHAPE, LATERAL_SPREAD)
+    n_nodes = w.shape[0]
+
+    g_w = G_BAR_EXAMPLE * G_W_STAR
+    g_x = G_BAR_EXAMPLE * G_X_STAR
+    g_d = G_BAR_EXAMPLE * G_D_STAR
+
+    run_time_example = 2 * len(DRIVEN_NODES)
+    run_time_prob = len(DRIVEN_NODES)
+
+    # make network
+
+    ntwk_example = network(w=w, g_w=g_w, g_x=g_x, g_d=g_d, t_x=T_X)
+
+    # make drive sequence
+
+    drives_example = np.zeros((run_time_example, n_nodes), dtype=float)
+
+    for t, node in enumerate(DRIVEN_NODES):
+        drives_example[t, node] = DRIVE_AMPLITUDE
+
+    drives_example[len(DRIVEN_NODES), DRIVEN_NODES[0]] = DRIVE_AMPLITUDE
+
+    r_0_example = np.zeros((n_nodes,))
+    xc_0_example = np.zeros((n_nodes,))
+    rs_example = ntwk_example.run(r_0=r_0_example, drives=drives_example, xc_0=xc_0_example)
+
+
+    # DO PROBABILITY CALCULATION
+
+    p_seqs = []
+
+    for g_bar in G_BARS:
+
+        g_w = g_bar * G_W_STAR
+        g_x = g_bar * G_X_STAR
+        g_d = g_bar * G_D_STAR
+
+        ntwk = network(w=w, g_w=g_w, g_x=g_x, g_d=g_d, t_x=T_X)
+
+        # set up initial state, initial hyperexcitability counter, and drives
+
+        r_0_prob = np.zeros((n_nodes,))
+
+        xc_0_prob = np.zeros((n_nodes,))
+        xc_0_prob[DRIVEN_NODES] = np.arange(T_X - run_time_prob, T_X) + 1
+
+        drives_prob = np.zeros((run_time_prob, n_nodes))
+        drives_prob[0, DRIVEN_NODES[0]] = DRIVE_AMPLITUDE
+
+        # calculate probability of network following sequence
+
+        p_seqs.append(ntwk.sequence_probability(DRIVEN_NODES, r_0_prob, xc_0_prob, drives_prob))
+
+
+    # MAKE PLOTS
+
+    fig, axs = plt.subplots(2, 1, figsize=FIG_SIZE, facecolor='white', tight_layout=True)
+
+    for ax_ctr, ax in enumerate(axs):
+
+        if ax_ctr == 0:
+
+            # example trace
+
+            fancy_raster(ax, rs_example, drives_example)
+
+            ax.set_ylim(0, n_nodes)
+
+            ax.set_xlabel('time step')
+            ax.set_ylabel('ensemble')
+
+        if ax_ctr == 1:
+
+            # probability vs g_bar
+
+            ax.plot(G_BARS, p_seqs, lw=2)
+
+            ax.set_ylim(0, 1)
+
+            ax.set_xlabel('gbar')
+            ax.set_ylabel('sequence replay \n probability')
 
         set_fontsize(ax, FONT_SIZE)
