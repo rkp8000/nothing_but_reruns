bb39c4009e36098c2048d1b02fa14b0ad31ceece
rkp8000 <rpang.contact@gmail.com>
2016-04-22 17:48:35 -0700

    Add function for detecting path replayability and for identifying all replayable and non-replayable paths.

--- metrics.py
+++ metrics.py
@@ -23,3 +23,67 @@ def paths_of_length(g, length):
             paths.extend([tuple(path) for path in src_targ_paths if len(path) == length])
 
     return paths
+
+
+def path_is_replayable(path, a):
+    """
+    Determine whether a path is replayable given an adjacency matrix. Note that in adjacency matrix
+    rows are targets, cols are sources. This is the reverse of the networkx adjacency matrix.
+
+    A path is defined as replayable if given the starting node as well as the set of other nodes
+    in the path (but not their order), the path can be uniquely reconstructed from the graph
+    connectivity.
+
+    Specifically, a path is defined as replayable is given the subnetwork defined by the nodes in the
+    path and their interconnections, no node in the path (except the last) has more than one node
+    downstream of it.
+
+    :param path: path to check (tuple)
+    :param a: adjacency matrix (rows are targs, cols are sources)
+    :return: True if path is replayable, False otherwise.
+    """
+
+    for k, src in enumerate(path[:-1]):
+
+        for targ in path[k + 2:]:
+
+            # return false if a downstream node is found
+            if a[targ, src] == 1:
+
+                return False
+
+    return True
+
+
+def replayable_paths(g, length, progress_ctr=0):
+    """
+    Return a list of all "replayable" paths in a graph. See documentation of "replayable" function for
+    definition of replayability.
+
+    :param g: graph
+    :param length: length of paths to look at
+    :return: List of replayable paths, list of nonreplayable paths
+    """
+
+    paths_all = paths_of_length(g, length)
+
+    paths_replayable = []
+    paths_non_replayable = []
+
+    for ctr, path in enumerate(paths_all):
+
+        if progress_ctr:
+
+            if ctr % progress_ctr == 0:
+
+                print('{} paths checked'.format(ctr))
+
+        if path_is_replayable(path, a):
+
+            paths_replayable.append(path)
+
+        else:
+
+            paths_non_replayable.append(path)
+
+    return paths_replayable, paths_non_replayable
