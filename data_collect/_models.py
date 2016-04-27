a9e6af27dd219647cddc1bd6be5b27ad8c3f2719
rkp8000 <rpang.contact@gmail.com>
2016-04-27 16:57:18 -0700

    Clean up ReplayCapacityNetwork model.

--- data_collect/_models.py
+++ data_collect/_models.py
@@ -7,16 +7,14 @@ Base = declarative_base()
 
 class ReplayCapacityNetwork(Base):
 
-    __tablename__ = 'replay_capacity_analysis'
+    __tablename__ = 'replay_capacity_network'
 
     id = Column(Integer, primary_key=True)
 
     graph_type = Column(String(100))
     creation_parameters = Column(JSONB)
-    n_nodes = Column(Integer)
-    n_edges = Column(Integer)
 
-    graph_object = Column(JSONB)
+    networkx_adjacency_matrix = Column(JSONB)
 
     sequence_lengths = Column(ARRAY(Integer))
     n_sequencess_replayable = Column(ARRAY(Integer))
