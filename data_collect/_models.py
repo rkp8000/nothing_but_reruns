35a29462cdbc98028bf64070dbdec3e0ac72dafb
rkp8000 <rpang.contact@gmail.com>
2016-04-27 12:32:26 -0700

    Add creation_parameters field to ReplayCapacityNetwork data model.

--- data_collect/_models.py
+++ data_collect/_models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column
-from sqlalchemy.dialects.postgresql import ARRAY, INTEGER, VARCHAR, JSONB
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,14 +9,15 @@ class ReplayCapacityNetwork(Base):
 
     __tablename__ = 'replay_capacity_analysis'
 
-    id = Column(INTEGER, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
-    graph_type = Column(VARCHAR(100))
-    n_nodes = Column(INTEGER)
-    n_edges = Column(INTEGER)
+    graph_type = Column(String(100))
+    creation_parameters = Column(JSONB)
+    n_nodes = Column(Integer)
+    n_edges = Column(Integer)
 
     graph_object = Column(JSONB)
 
-    sequence_lengths = Column(ARRAY(INTEGER))
-    n_sequencess_replayable = Column(ARRAY(INTEGER))
-    n_sequencess_non_replayable = Column(ARRAY(INTEGER))
+    sequence_lengths = Column(ARRAY(Integer))
+    n_sequencess_replayable = Column(ARRAY(Integer))
+    n_sequencess_non_replayable = Column(ARRAY(Integer))
