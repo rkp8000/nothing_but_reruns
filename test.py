9f6bac28efad2a0973c1588fef550201e98e8183
rkp8000 <rpang.contact@gmail.com>
2016-05-04 13:52:15 -0700

    Test transition-counting function.

--- test.py
+++ test.py
@@ -180,6 +180,24 @@ class MetricsTestCase(unittest.TestCase):
 
             self.assertFalse(metrics.path_is_replayable(path, a))
 
+    def test_transition_count_gives_correct_example_results(self):
+
+        seq = [0, 1, 3, 2, 4, 3, 1, 2, 0, 3, 0, 2, 0, 4, 1, 2, 1, 2, 1, 1]
+
+        transitions_correct = np.array([
+            [0, 0, 2, 1, 0],
+            [1, 1, 2, 1, 1],
+            [1, 3, 0, 1, 0],
+            [1, 1, 0, 0, 1],
+            [1, 0, 1, 0, 0],
+        ], dtype=float)
+
+        states = np.arange(5)
+
+        transitions = metrics.transition_count(seq, states)
+
+        np.testing.assert_array_almost_equal(transitions, transitions_correct)
+
     def test_transition_matrix_DKL_gives_correct_example_results(self):
 
         # define transition matrices and stationary distributions
