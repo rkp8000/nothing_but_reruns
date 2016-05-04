0772974ee882af86721e5ebb913ab1c28671cd19
rkp8000 <rpang.contact@gmail.com>
2016-05-04 12:45:13 -0700

    Add function for making directed er matrices with binary or n-ary connection strengths.

--- connectivity.py
+++ connectivity.py
@@ -1,4 +1,5 @@
 from __future__ import division, print_function
+import networkx as nx
 import numpy as np
 
 
@@ -20,6 +21,7 @@ def feed_forward_grid(shape, spread):
     w = np.zeros((n_nodes, n_nodes))
 
     for row in range(shape[0]):
+
         for col in range(shape[1] - 1):
 
             # get multi indexes of downstream nodes
@@ -40,6 +42,41 @@ def feed_forward_grid(shape, spread):
             src = np.ravel_multi_index((row, col), shape)
 
             for targ in downstream_flats:
+
                 w[targ, src] = 1
+
+    return w
+
+
+def er_directed(n_nodes, p_connect):
+    """
+    Construct a directed Erdos-Renyi network (with binary connection weights). This is just a wrapper
+    around networkx's erdos_renyi_network function.
+
+    :param n_nodes: number of nodes
+    :param p_connect: connection probability
+    :return: weight matrix (rows are targs, cols are srcs)
+    """
+
+    g = nx.erdos_renyi_graph(n_nodes, p_connect, directed=True)
+
+    return np.array(nx.adjacency_matrix(g).T.todense())
+
+
+def er_directed_nary(n_nodes, p_connect, strengths, p_strengths):
+    """
+    Construct a directed Erdos-Renyi network (with binary connection weights).
+
+    :param n_nodes: number of nodes
+    :param p_connect: connection probability
+    :param strengths: list of strengths that weights can take
+    :param p_strengths: probabilities that connections take on each strength
+    :return: weight matrix (rows are targs, cols are srcs)
+    """
+
+    w = (np.random.rand(n_nodes, n_nodes) < p_connect).astype(float)
+    np.fill_diagonal(w, 0)
+
+    w[w > 0] = np.random.choice(strengths, size=(w.sum(),), p=p_strengths)
 
     return w
