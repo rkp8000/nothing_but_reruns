df2058b914ec4b8b6710ff7a48b50fe19e54e170
rkp8000 <rpang.contact@gmail.com>
2016-06-08 14:48:47 -0700

    Add more functionality for multiple drive trains, etc.

--- figures/lif_implementation.py
+++ figures/lif_implementation.py
@@ -13,14 +13,17 @@ def sequence_replay_branching_chain_demo(
         CHAIN_LENGTH_PRE, CHAIN_LENGTH_POST,
         W_PP, W_PI, W_PM, W_IP, W_II, W_MP, W_MM,
         BKGD_STARTS, BKGD_ENDS, BKGD_FRQS, BKGD_STRENS,
-        DRIVE_STARTS, DRIVE_STRENS, DRIVE_DURS, DRIVE_FRQS, DRIVE_ITVS,
+        DRIVE_ORDERS, DRIVE_STARTS, DRIVE_STRENS, DRIVE_DURS, DRIVE_FRQS, DRIVE_ITVS,
         REPLAY_TRIGGER_TIMES, REPLAY_TRIGGER_STRENS,
+        MEMORY_RESET_STARTS, MEMORY_RESET_ENDS, MEMORY_RESET_STRENS, MEMORY_RESET_FRQS,
         SIM_DURATION, DT,
         FIG_SIZE, FONT_SIZE):
 
     syns = TAUS_SYN.keys()
 
     # build drive arrays
+
+    np.random.seed(SEED)
 
     n_steps = int(SIM_DURATION / DT)
     n_primary_cells = CHAIN_LENGTH_PRE + 2 * CHAIN_LENGTH_POST
@@ -30,53 +33,78 @@ def sequence_replay_branching_chain_demo(
 
     for syn in syns:
 
-        # drive one path through network with a sequence of volleys
+        for drive_ctr in range(len(DRIVE_ORDERS)):
 
-        start = DRIVE_STARTS[syn]
-        dur = DRIVE_DURS[syn]
-        frq = DRIVE_FRQS[syn]
-        itv = DRIVE_ITVS[syn]
+            # drive one path through network with a sequence of volleys
 
-        if frq > 0:
+            start = DRIVE_STARTS[drive_ctr][syn]
+            dur = DRIVE_DURS[drive_ctr][syn]
+            frq = DRIVE_FRQS[drive_ctr][syn]
+            itv = DRIVE_ITVS[drive_ctr][syn]
 
-            period = 1. / frq
+            if frq > 0:
 
-        else:
+                period = 1. / frq
 
-            period = np.inf
+            else:
 
-        for cell_ctr in range(CHAIN_LENGTH_PRE + CHAIN_LENGTH_POST):
+                period = np.inf
 
-            volley_start = start + cell_ctr * itv
-            volley_end = volley_start + dur
-            volley_times = np.arange(volley_start, volley_end, period)
+            for cell_ctr, cell in enumerate(DRIVE_ORDERS[drive_ctr]):
 
-            volley_times_idx = np.round(volley_times / DT).astype(int)
+                volley_start = start + cell_ctr * itv
+                volley_end = volley_start + dur
+                volley_times = np.arange(volley_start, volley_end, period)
 
-            drives[syn][volley_times_idx, cell_ctr] = DRIVE_STRENS[syn]
+                volley_times_idx = np.round(volley_times / DT).astype(int)
 
-        # drive first neuron with replay trigger
+                drives[syn][volley_times_idx, cell] += DRIVE_STRENS[drive_ctr][syn]
 
-        replay_trigger_time = REPLAY_TRIGGER_TIMES[syn]
-        replay_trigger_time_idx = np.round(replay_trigger_time / DT).astype(int)
+            # drive first neuron with replay trigger
 
-        drives[syn][replay_trigger_time_idx, 0] = REPLAY_TRIGGER_STRENS[syn]
+            replay_trigger_time = REPLAY_TRIGGER_TIMES[drive_ctr][syn]
+            replay_trigger_time_idx = np.round(replay_trigger_time / DT).astype(int)
+
+            drives[syn][replay_trigger_time_idx, DRIVE_ORDERS[drive_ctr][0]] += \
+                REPLAY_TRIGGER_STRENS[drive_ctr][syn]
+
+        for mr_ctr in range(len(MEMORY_RESET_STARTS)):
+
+            # reset memory units
+
+            mr_start = MEMORY_RESET_STARTS[mr_ctr][syn]
+            mr_end = MEMORY_RESET_ENDS[mr_ctr][syn]
+            mr_stren = MEMORY_RESET_STRENS[mr_ctr][syn]
+            mr_frq = MEMORY_RESET_FRQS[mr_ctr][syn]
+
+            if mr_frq > 0:
+
+                mr_period = 1. / mr_frq
+
+            else:
+
+                mr_period = np.inf
+
+            mr_times = np.arange(mr_start, mr_end, mr_period)
+            mr_times_idx = np.round(mr_times / DT).astype(int)
+
+            drives[syn][mr_times_idx, n_primary_cells:2*n_primary_cells] += mr_stren
 
         # add background
 
-        bkgd_start_idx = int(BKGD_STARTS[syn] / DT)
-        bkgd_end_idx = int(BKGD_ENDS[syn] / DT)
-        bkgd_frq = BKGD_FRQS[syn]
-        bkgd_stren = BKGD_STRENS[syn]
+        for bkgd_ctr in range(len(BKGD_STARTS)):
 
-        bkgd = bkgd_stren * np.random.poisson(
-            bkgd_frq * DT, (bkgd_end_idx - bkgd_start_idx, n_primary_cells))
+            bkgd_start_idx = int(BKGD_STARTS[bkgd_ctr][syn] / DT)
+            bkgd_end_idx = int(BKGD_ENDS[bkgd_ctr][syn] / DT)
+            bkgd_frq = BKGD_FRQS[bkgd_ctr][syn]
+            bkgd_stren = BKGD_STRENS[bkgd_ctr][syn]
 
-        drives[syn][bkgd_start_idx:bkgd_end_idx, :n_primary_cells] += bkgd
+            bkgd = bkgd_stren * np.random.poisson(
+                bkgd_frq * DT, (bkgd_end_idx - bkgd_start_idx, n_primary_cells))
+
+            drives[syn][bkgd_start_idx:bkgd_end_idx, :n_primary_cells] += bkgd
 
     # build weight matrices
-
-    np.random.seed(SEED)
 
     ws = {syn: np.zeros((n_cells, n_cells)) for syn in syns}
 
@@ -155,36 +183,37 @@ def sequence_replay_branching_chain_demo(
 
     axs[0].set_ylabel('neuron')
 
-    # primary spikes
-
-    primary_spikes = measurements['spikes'][:, :n_primary_cells].nonzero()
-
-    axs[1].scatter(primary_spikes[0] * DT, primary_spikes[1], s=200, marker='|', c='k')
-
-    axs[1].set_ylim(-1, n_primary_cells)
-    axs[1].set_ylabel('neuron')
-
-    memory_voltages = measurements['voltages'][:, n_primary_cells:2*n_primary_cells]
+    memory_voltages = measurements['voltages'][:, n_primary_cells:2 * n_primary_cells]
 
     scales = [V_TH - V_REST]
     spacing = 2
     colors = ['b']
 
     multivariate_same_axis(
-        axs[2], measurements['time'], [memory_voltages], scales, spacing, colors)
+        axs[1], measurements['time'], [memory_voltages], scales, spacing, colors)
 
+    axs[1].set_ylabel('neuron')
+
+    # primary spikes
+
+    primary_spikes = measurements['spikes'][:, :n_primary_cells].nonzero()
+
+    axs[2].scatter(primary_spikes[0] * DT, primary_spikes[1], s=200, marker='|', c='k', lw=1)
+
+    axs[2].set_ylim(-1, n_primary_cells)
     axs[2].set_ylabel('neuron')
 
     # memory spikes
 
     memory_spikes = measurements['spikes'][:, n_primary_cells:2*n_primary_cells].nonzero()
 
-    axs[3].scatter(memory_spikes[0] * DT, memory_spikes[1], s=200, marker='|', c='k')
+    axs[3].scatter(memory_spikes[0] * DT, memory_spikes[1], s=150, marker='|', c='b', lw=1)
 
+    axs[3].set_xlim(0, SIM_DURATION)
     axs[3].set_ylim(-1, n_primary_cells)
-    axs[3].set_ylabel('neuron')
 
     axs[3].set_xlabel('time (s)')
+    axs[3].set_ylabel('neuron')
 
     for ax in axs:
 
