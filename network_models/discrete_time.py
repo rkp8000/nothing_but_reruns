ea42d6b5d36408aa98e2325493ca028aec98c8cf
rkp8000 <rpang.contact@gmail.com>
2016-11-10 14:30:51 -0800

    Add tested classes to include STDP in nodes and local wta rules.

--- network_models/discrete_time.py
+++ network_models/discrete_time.py
@@ -1,5 +1,8 @@
 from __future__ import division, print_function
 from copy import copy
+from itertools import combinations
+from itertools import product as cproduct
+import networkx as nx
 import numpy as np
 
 
@@ -14,358 +17,172 @@ def _calculate_softmax_probability(inputs):
     return prob / prob.sum()
 
 
-class BasicWithLingeringHyperexcitability(object):
+class BasicWithAthAndTwoLevelStdp(object):
     """
     Most basic model with activation-triggered lingering hyperexcitability.
     """
 
-    def __init__(self, th, w, g_x, t_x):
+    def __init__(self, th, w, g_x, t_x, rp, stdp_params):
+        """
+        :param th: input threshold above which node activates
+        :param w: weight matrix
+        :param g_x: hyperexcitability level
+        :param t_x: hyperexcitability timescale
+        :param rp: refractory period
+        :param stdp_params: dictionary of stdp params:
+            :param 'w_1': weak synaptic strength
+            :param 'w_2': strong synaptic strength
+            :param 'alpha_1': learning rate towards w_1
+            :param 'alpha_2': learning rate towards w_2
+        """
 
         self.th = th
         self.w = w
         self.g_x = g_x
-        self.p_relax = 1. / (1 + t_x)
+        self.t_x = t_x
+        self.rp = rp
+        self.th = th
 
         self.n_nodes = w.shape[0]
 
-    def run(self, r_0, xc_0, drives, noise_level):
+        self.w_1 = 0 if stdp_params is None else stdp_params['w_1']
+        self.w_2 = 0 if stdp_params is None else stdp_params['w_2']
+        self.alpha_1 = 0 if stdp_params is None else stdp_params['alpha_1']
+        self.alpha_2 = 0 if stdp_params is None else stdp_params['alpha_2']
+
+    def update_weights(self, w, r_prev, r):
+        """
+        Update a weight matrix according to the STDP learning rate.
+        Note that w is modified in place!
+        :param w: previous weight matrix
+        :param r_prev: previous unit activations
+        :param r: current unit activations
+        :return: updated weight matrix
+        """
+
+        if self.alpha_1 == self.alpha_2 == 0: return w
+
+        # loop through all sequential activations
+        for prev, curr in cproduct(r_prev.nonzero()[0], r.nonzero()[0]):
+
+            if w[prev, curr]: w[prev, curr] += self.alpha_1*(self.w_1 - w[prev, curr])
+            if w[curr, prev]: w[curr, prev] += self.alpha_2*(self.w_2 - w[curr, prev])
+
+        return w
+
+    def adjust_for_local_wta(self, r): return r
+
+    def run(self, r_0, xc_0, drives, measure_w=None):
         """
         Run the network from a starting state by providing a stimulus.
+        :param r_0: initial node activations
+        :param xc_0: initial hyperexcitability states
+        :param drives: stimuli for all nodes
+        :param measure_w: function that takes in weight matrix as a single argument
+            and outputs a quantity that will be stored in a list of measurements
         """
 
         rs = np.nan * np.zeros((self.n_nodes, len(drives)))
         xcs = np.nan * np.zeros((self.n_nodes, len(drives)))
 
+        # set state for first time step
         r = r_0.copy()
         xc = xc_0.copy()
+        rpc = np.zeros((self.n_nodes,))
+
+        w = self.w.copy()
+        w_measurements = []
 
         for t, drive in enumerate(drives):
 
             if t > 0:
 
+                r_prev = r.copy()
+
                 # calculate inputs and compare them to threshold
-                noise = noise_level * np.random.normal(0, 1, self.n_nodes)
-                r = (self.w.dot(r) + drive + self.g_x*xc + noise > self.th).astype(int)
+                x = (xc > 0).astype(float)
+                r = (w.dot(r) + drive + self.g_x*x > self.th).astype(int)
+                # remove active nodes in refractory period
+                r[rpc > 0] = 0
 
-            # store active units and make them hyperexcitable
+                w = self.update_weights(w, r_prev, r)
+
+            # remove (nonexistent) WTA violations
+            r = self.adjust_for_local_wta(r)
+
+            # decrement hyperexcitabilities and refractory counter
+            xc[xc > 0] -= 1
+            rpc[rpc > 0] -= 1
+
+            # make new active units hyperexcitable and refractory
+            xc[r > 0] = self.t_x
+            rpc[r > 0] = self.rp
+
+            # store activities and hyperexcitabilities
             rs[:, t] = r
-            xc[r > 0] = 1
-
-            # probabilistically turn off hyperexcitabilities
-            xc[np.random.rand(self.n_nodes) < self.p_relax] = 0
-
             xcs[:, t] = xc.copy()
 
-        return rs.T, xcs.T
+            if measure_w is not None:
+                w_measurements.append(measure_w(w))
+
+        if measure_w is None:
+            return rs.T, xcs.T
+        else:
+            return rs.T, xcs.T, w_measurements
 
 
-class SoftmaxWTAWithLingeringHyperexcitability(object):
-    """
-    Network model with activation-dependent excitability changes and winner-take-all
-    rule enforcing one node active per timestep.
+class LocalWtaWithAthAndStdp(BasicWithAthAndTwoLevelStdp):
 
-    In this network, exactly one node is allowed to be active at each time step. Which node becomes active
-    is chosen with a probability that depends exponentially on its input (i.e., according to the softmax function).
+    def __init__(self, th, w, g_x, t_x, rp, stdp_params, wta_dist):
 
-    The possible inputs to the node at a current time step, which are weighted by their respective gains, are:
-        upstream (from active node at last time step)
-        lingering excitability (excitability changes are represented as inputs)
-        external drive
+        super(self.__class__, self).__init__(th, w, g_x, t_x, rp, stdp_params)
 
-    :param w: weight matrix (rows are targets, cols are sources)
-    :param g_w: gain on upstream inputs
-    :param g_x: gain on lingering hyperexcitability
-    :param g_d: gain on external drive
-    :param t_x: timescale of lingering hyperexcitability
-    """
+        self.wta_dist = wta_dist
+        self.node_distances = np.zeros(w.shape)
 
-    def __init__(self, w, g_w, g_x, g_d, t_x):
-        self.w = w
-        self.g_w = g_w
-        self.g_x = g_x
-        self.g_d = g_d
-        self.t_x = t_x
+        # use networkx to get shortest path length dict
+        # and fill in shortest paths in the node distances matrix
 
-        self.n_nodes = w.shape[0]
+        g = nx.Graph((self.w + self.w.T > 0).astype(int))
 
-    def calculate_inputs(self, r, xc, drive):
+        for node_0, spls in nx.shortest_path_length(g).items():
+            for node_1, spl in spls.items():
+
+                self.node_distances[node_0, node_1] = spl
+                self.node_distances[node_1, node_0] = spl
+
+    def adjust_for_local_wta(self, r):
         """
-        Calculate vector of inputs to network given previous activation state, hyperexcitability counter, and drive.
-        :param r: activation state at last time step
-        :param xc: hyperexcitability counter
-        :param drive: external drive
-        :return: vector of inputs
+        Ensure that no two nodes are active if they are <= self.wta_distance from each other
+        :param r: candidate activation vector (prior to WTA correction)
+        :return: corrected activation vector
         """
 
-        upstream = self.w.dot(r)
-        excitability = (xc > 0).astype(float)
+        active_nodes = list(r.nonzero()[0])
 
-        return self.g_w * upstream + self.g_x * excitability + self.g_d * drive
+        # get all pairs of nodes that are too close together
+        invalid_pairs = []
+        for pair in combinations(active_nodes, 2):
 
-    def run(self, r_0, xc_0, drives):
-        """
-        Run the network under a set of external drives.
+            if 0 < self.node_distances[pair[0], pair[1]] <= self.wta_dist:
+                invalid_pairs.append(pair)
 
-        :param r_0: initial activation state
-        :param xc_0: initial hyperexcitability counter
-        :param drives: external drives at each time point
-        :return: network activation at each time point
-        """
+        # turn off nodes with probability proportional to how many invalid
+        # pairs they participate in until there are no more invalid pairs
+        while invalid_pairs:
 
-        r = copy(r_0)
-        xc = copy(xc_0)
+            flat = list(np.array(invalid_pairs).flat)
+            node_counts = np.array([flat.count(node) for node in active_nodes])
+            probs = node_counts / node_counts.sum()
 
-        rs = []
+            to_inactivate = np.random.choice(active_nodes, p=probs)
 
-        for drive in drives:
+            active_nodes.pop(active_nodes.index(to_inactivate))
+            invalid_pairs = [pair for pair in invalid_pairs if to_inactivate not in pair]
 
-            # calculate inputs and softmax probabilities
-            inputs = self.calculate_inputs(r, xc, drive)
-            prob = _calculate_softmax_probability(inputs)
+        if r.sum(): assert len(active_nodes) > 0
 
-            # select active node
-            active_node = np.random.choice(range(self.n_nodes), p=prob)
-            r = np.zeros((self.n_nodes,))
-            r[active_node] = 1.
+        r_adjusted = np.zeros(r.shape)
+        r_adjusted[active_nodes] = 1
 
-            rs.append(r)
-
-            # decrement hyperexcitability counters
-            xc[xc > 0] -= 1
-            # turn on hyperexcitability of last active node
-            xc[active_node] = self.t_x
-
-        return np.array(rs)
-
-    def sequence_probability(self, seq, r_0, xc_0, drives):
-        """
-        Calculate the probability of the network producing a sequence given an initial state and
-        set of drives.
-        :param seq: sequence to be produced (1-D array)
-        :param r_0, xc_0, drives: see method "run"
-        :return: probability of network producing sequence
-        """
-
-        p_seq = 1
-
-        r = copy(r_0)
-        xc = copy(xc_0)
-
-        for candidate_node, drive in zip(seq, drives):
-
-            # calculate inputs and softmax probabilities
-            inputs = self.calculate_inputs(r, xc, drive)
-            prob = _calculate_softmax_probability(inputs)
-
-            # get probability of candidate node activating
-            p_seq *= prob[candidate_node]
-
-            # update activations and hyperexcitability counters as if the candidate node had become active,
-            # so that the next probability can be calculated conditioned on previous part of the sequence
-            r = np.zeros((self.n_nodes,))
-            r[candidate_node] = 1.
-
-            # decrement hyperexcitability counters
-            xc[xc > 0] -= 1
-            # turn on hyperexcitability of last active node
-            xc[candidate_node] = self.t_x
-
-        return p_seq
-
-
-class SoftmaxWTAWithLingeringHyperexcitabilityAndRefractoriness(object):
-    """
-    Most basic network model with activation-dependent excitability changes.
-
-    In this network, exactly one node is allowed to be active at each time step. Which node becomes active
-    is chosen with a probability that depends exponentially on its input (i.e., according to the softmax function).
-
-    The possible inputs to the node at a current time step, which are weighted by their respective gains, are:
-        upstream (from active node at last time step)
-        lingering excitability (excitability changes are represented as inputs)
-        external drive
-
-    :param w: weight matrix (rows are targets, cols are sources)
-    :param g_w: gain on upstream inputs
-    :param g_x: gain on lingering hyperexcitability
-    :param g_d: gain on external drive
-    :param t_x: timescale of lingering hyperexcitability
-    :param g_r: gain on refractoriness (this should be negative to be biological)
-    :param t_r: timescale of refractoriness
-    """
-
-    def __init__(self, w, g_w, g_x, g_d, t_x, g_rf, t_rf):
-        self.w = w
-        self.g_w = g_w
-        self.g_x = g_x
-        self.g_d = g_d
-        self.t_x = t_x
-        self.g_rf = g_rf
-        self.t_rf = t_rf
-
-        self.n_nodes = w.shape[0]
-
-    def calculate_inputs(self, r, xc, rfc, drive):
-        """
-        Calculate vector of inputs to network given previous activation state, hyperexcitability counter, and drive.
-        :param r: activation state at last time step
-        :param xc: hyperexcitability counter
-        :param rfc: refractoriness counter
-        :param drive: external drive
-        :return: vector of inputs
-        """
-
-        upstream = self.w.dot(r)
-        excitability = (xc > 0).astype(float)
-        refractoriness = (rfc > 0).astype(float)
-
-        inputs = self.g_w * upstream + self.g_x * excitability + \
-            self.g_rf * refractoriness + self.g_d * drive
-
-        return inputs
-
-    def run(self, r_0, xc_0, rfc_0, drives):
-        """
-        Run the network under a set of external drives.
-
-        :param r_0: initial activation state
-        :param xc_0: initial hyperexcitability counter
-        :param drives: external drives at each time point
-        :return: network activation at each time point
-        """
-
-        r = copy(r_0)
-        xc = copy(xc_0)
-        rfc = copy(rfc_0)
-
-        rs = []
-
-        for drive in drives:
-
-            # calculate inputs and softmax probabilities
-            inputs = self.calculate_inputs(r, xc, rfc, drive)
-            prob = _calculate_softmax_probability(inputs)
-
-            # select active node
-            active_node = np.random.choice(range(self.n_nodes), p=prob)
-            r = np.zeros((self.n_nodes,))
-            r[active_node] = 1.
-
-            rs.append(r)
-
-            # decrement hyperexcitability counters
-            xc[xc > 0] -= 1
-
-            # decrement refractoriness counters
-            rfc[rfc > 0] -= 1
-
-            # turn on hyperexcitability of last active node
-            xc[active_node] = self.t_x
-
-            # turn on refractoriness of last active node
-            rfc[active_node] = self.t_rf
-
-        return np.array(rs)
-
-
-class SoftmaxWTAWithLingeringHyperexcitabilityAndSTDP(object):
-    """
-    Similar to the class it inherits from, except that it includes "poor-man's" STDP.
-    """
-
-    def __init__(self, w, g_w, g_x, g_d, t_x, w_max, alpha):
-
-        self.w = w
-        self.g_w = g_w
-        self.g_x = g_x
-        self.g_d = g_d
-        self.t_x = t_x
-
-        self.w_max = w_max
-        self.alpha = alpha
-
-        self.n_nodes = w.shape[0]
-
-    def calculate_inputs(self, r, xc, drive, w):
-        """
-        Calculate vector of inputs to network given previous activation state, hyperexcitability counter, and drive.
-        :param r: activation state at last time step
-        :param xc: hyperexcitability counter
-        :param drive: external drive
-        :param w: weight matrix
-        :return: vector of inputs
-        """
-
-        upstream = w.dot(r)
-        excitability = (xc > 0).astype(float)
-
-        return self.g_w * upstream + self.g_x * excitability + self.g_d * drive
-
-    def run(self, r_0, xc_0, drives, weight_measurement_function=None):
-        """
-        Run the network under a set of external drives.
-
-        :param r_0: initial activation state
-        :param xc_0: initial hyperexcitability counter
-        :param drives: external drives at each time point
-        :param weight_measurement_function: function to measure some aspect of the weight matrix
-        :return: network activation at each time point
-        """
-
-        r = copy(r_0)
-        xc = copy(xc_0)
-
-        rs = []
-
-        w = copy(self.w)
-
-        w_measurements = []
-
-        for drive in drives:
-
-            previous_node = r.argmax()
-
-            # calculate inputs and softmax probabilities
-
-            inputs = self.calculate_inputs(r, xc, drive, w)
-            prob = _calculate_softmax_probability(inputs)
-
-            # select active node
-
-            active_node = np.random.choice(range(self.n_nodes), p=prob)
-            r = np.zeros((self.n_nodes,))
-            r[active_node] = 1.
-
-            rs.append(r)
-
-            # decrement hyperexcitability counters
-
-            xc[xc > 0] -= 1
-
-            # turn on hyperexcitability of active node
-
-            xc[active_node] = self.t_x
-
-            # update weight in accordance with previously and currently active nodes
-
-            if w[active_node, previous_node] > 0:
-
-                # get normalization factor
-
-                norm_factor = w[:, previous_node].sum()
-
-                # increase relevant connection in STDP-like way
-
-                w[active_node, previous_node] += (self.alpha * (self.w_max - w[active_node, previous_node]))
-
-                # normalize outgoing connections
-
-                w[:, previous_node] *= (norm_factor / w[:, previous_node].sum())
-
-            # take measurement
-
-            if weight_measurement_function is not None:
-
-                w_measurements.append(weight_measurement_function(w))
-
-        return np.array(rs), w_measurements
+        return r_adjusted
