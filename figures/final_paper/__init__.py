884d6a5d4f4b1c2f75f8bfab898e49ee669ddf82
rkp8000 <rpang.contact@gmail.com>
2016-11-17 16:07:41 -0800

    Write untested function to record all spontaneous replay extension results to database.

--- figures/final_paper/__init__.py
+++ figures/final_paper/__init__.py
@@ -1,14 +1,128 @@
 from __future__ import division, print_function
+import logging
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+import threading
 
 import connectivity
+from db import _models
+import db
 import network
 import plot
 
 
-def extended_replay_noise_dependence(
+def record_extension_by_spontaneous_replay(
+        SEED, GROUP_NAME, LOG_FILE,
+        NETWORK_SIZE, V_TH, RP, T_X,
+        NODE_SEQ, DRIVE_AMP, PROBE_TIME,
+        ALPHA, G_X_STEP, G_W_STEP, NOISE_STEP,
+        N_TRIALS, LOW_PROB_THRESHOLD, LOW_PROB_MIN_TRIALS,):
+    """
+    Perform a parameter sweep over varying influences of the hyperexcitability
+    and the connection weight term and save the results to the database.
+    """
+
+    # preliminaries
+    np.random.seed(SEED)
+    s = db.connect_and_make_session('neural_pattern_replay')
+    m = _models.SpontaneousReplayExtensionResult
+    db.delete_record_group(s, m.group, GROUP_NAME)
+
+    db.prepare_logging(LOG_FILE)
+
+    g_xs = np.arange(0, V_TH, G_X_STEP)[1:]
+
+    # make weight matrix
+    w_base, nodes = connectivity.hexagonal_lattice(NETWORK_SIZE)
+
+    # make pre-noise drives
+    l = len(NODE_SEQ)
+    drives_base = np.zeros((2*l + PROBE_TIME, len(nodes)))
+
+    for ctr, node in NODE_SEQ: drives_base[ctr + 1, nodes.index(node)] = DRIVE_AMP
+    drives_base[PROBE_TIME + 1, nodes.index(NODE_SEQ[0])] = DRIVE_AMP
+
+    node_seq_logical = drives_base[1:1+l, :]
+
+    r_0 = np.zeros((len(nodes),))
+    xc_0 = np.zeros((len(nodes),))
+
+    for g_x in g_xs:
+
+        logging.info('Starting sweep set with g_x = {0:.3f}.'.format(g_x))
+
+        # the max noise we'll consider is one in which there is a
+        # 20% chance that a hyperexcitable node activates spontaneously
+        noise_std_max = (1/stats.norm.ppf(0.8)) * (v_th - g_x)
+        noise_stds = np.arange(0, noise_std_max, NOISE_STEP)
+
+        # make sure we actually have a nonzero noise to test
+        if len(noise_stds) > 1: noise_stds = noise_stds[1:]
+        else: continue
+
+        # loop over all possible values of g_w
+        g_ws = np.arange(V_TH-g_x, V_TH, G_W_STEP)
+        for g_w in g_ws:
+
+            logging.info('Starting sweep with g_x = {0:.3f}, g_w = {1:.3f}.'.format(g_x, g_w))
+            logging.info('Sweeping over {} noise levels...'.format(len(noise_stds)))
+
+            ntwk = network.LocalWtaWithAthAndStdp(
+                th=v_th, w=g_w*w_base, g_x=g_x, t_x=t_x, rp=rp,
+                stdp_params=None, wta_dist=2, wta_factor=ALPHA)
+
+            # set up our data structure
+            sper = _models.SpontaneousReplayExtensionResult(
+                group=GROUP_NAME,
+                network_size=NETWORK_SIZE,
+                v_th=V_TH, rp=RP, t_x=T_X,
+                sequence=NODE_SEQ,
+                drive_amplitude=DRIVE_AMP,
+                probe_time=PROBE_TIME,
+                n_trials_attempted=N_TRIALS,
+                low_probability_threshold=LOW_PROB_THRESHOLD,
+                low_probability_min_trials=LOW_PROB_MIN_TRIALS,
+
+                alpha=ALPHA,
+                g_x=g_x,
+                g_w=g_w,
+                noise_stds=noise_stds,
+                probed_replay_probs=[],
+                n_trials_completed=[])
+
+            for noise_std in noise_stds:
+
+                replay_successes = []
+                for tr_ctr in range(N_TRIALS):
+
+                    # make noisy drives and run network
+                    drives = drives_base + noise_std * np.random.randn(*drives_base.shape)
+                    rs = ntwk.run(r_0, xc_0, drives)[0]
+
+                    # compare initial and probed replay sequence to true sequence
+                    initial_match = np.all(rs.T[1:1+l] == node_seq_logical)
+                    replay_match = np.all(rs.T[PROBE_TIME+1:PROBE_TIME+1+l] == node_seq_logical)
+
+                    replay_successes.append(initial_match and replay_match)
+
+                    # skip remaining trials if estimated probability is sufficiently small
+                    if tr_ctr + 1 >= LOW_PROB_MIN_TRIALS:
+                        if np.mean(replay_successes) < LOW_PROB_THRESHOLD:
+                            break
+
+                sper.probed_replay_probs.append(np.mean(replay_successes))
+                sper.n_trials_completed.append(tr_ctr + 1)
+
+            session.add(sper)
+            session.commit()
+
+        logging.info('All sweeps completed.')
+    session.close()
+
+
+def extension_by_spontaneous_replay(
         SEEDS_EXAMPLE, NOISES_EXAMPLE,
         SEED_STATS, NOISES_STATS, N_TRIALS,
         D, W, TH, G_X, T_X, RP, WS_STATS, G_XS_STATS,
