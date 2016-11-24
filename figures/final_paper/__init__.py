33ee23a64ece1152893235045cf769f7e0686f8a
rkp8000 <rpang.contact@gmail.com>
2016-11-23 17:00:25 -0800

    Make plot to show results of spontaneous replay extension analysis.

--- figures/final_paper/__init__.py
+++ figures/final_paper/__init__.py
@@ -45,7 +45,6 @@ def record_extension_by_spontaneous_replay(
 
     node_seq_logical = (drives_base[1:1+l] > 0).astype(int)
 
-    print(*node_seq_logical.nonzero())
     r_0 = np.zeros((len(nodes),))
     xc_0 = np.zeros((len(nodes),))
 
@@ -54,8 +53,8 @@ def record_extension_by_spontaneous_replay(
         logging.info('Starting sweep set with g_x = {0:.3f}.'.format(g_x))
 
         # the max noise we'll consider is one in which there is a
-        # 30% chance that a hyperexcitable node activates spontaneously
-        noise_std_max = (1/stats.norm.ppf(0.7)) * (V_TH - g_x)
+        # 20% chance that a hyperexcitable node activates spontaneously
+        noise_std_max = (1/stats.norm.ppf(0.8)) * (V_TH - g_x)
         noise_stds = [ns for ns in NOISE_STDS if 0 <= ns < noise_std_max]
 
         # make sure we actually have a nonzero noise to test
@@ -91,7 +90,7 @@ def record_extension_by_spontaneous_replay(
                 probed_replay_probs=[],
                 n_trials_completed=[])
 
-            for noise_std in noise_stds:
+            for ns_ctr, noise_std in enumerate(noise_stds):
 
                 broken = False
 
@@ -121,6 +120,9 @@ def record_extension_by_spontaneous_replay(
 
                 sper.n_trials_completed.append(tr_ctr + 1)
 
+                if (ns_ctr + 1) % 5 == 0:
+                    logging.info('{} noise levels completed.'.format(ns_ctr + 1))
+
             session.add(sper)
             session.commit()
 
@@ -130,44 +132,97 @@ def record_extension_by_spontaneous_replay(
 
 def extension_by_spontaneous_replay(
         SEEDS_EXAMPLE, NOISES_EXAMPLE,
-        SEED_STATS, NOISES_STATS, N_TRIALS,
-        D, W, TH, G_X, T_X, RP, WS_STATS, G_XS_STATS,
-        NODE_SEQ, REPLAY_TRIGGER, DRIVE_AMP):
+        W, G_X, GROUP_NAME, G_XS, X_LIM, Y_LIM):
     """
     Make plots that explore the dependence of extended replay on
     spontaneous noise level.
     """
-    w_base, nodes = connectivity.hexagonal_lattice(D)
+    # preliminaries
+    session = db.connect_and_make_session('nothing_but_reruns')
+    m = _models.SpontaneousReplayExtensionResult
+
+    gs = gridspec.GridSpec(1 + len(NOISES_EXAMPLE), len(G_XS))
+    fig_size = (15, 3 * (1 + len(NOISES_EXAMPLE)))
+
+    fig = plt.figure(figsize=fig_size, tight_layout=True)
+    axs = []
+
+    # plot statistics
+
+    max_prob = np.max([
+        np.max(srer.probed_replay_probs)
+        for srer in session.query(m).filter(m.group == GROUP_NAME).all()
+    ])
+
+    print('Max replay prob = {}'.format(max_prob))
+
+    for ctr, g_x in enumerate(G_XS):
+
+        srers = session.query(m).filter(
+            m.group == GROUP_NAME,
+            m.g_x.between(0.999*g_x, 1.001*g_x)).order_by(m.g_w).all()
+
+        axs.append(fig.add_subplot(gs[0, ctr]))
+
+        results = []
+
+        for srer in srers:
+            xs = srer.g_w * np.ones((len(srer.noise_stds),))
+            ys = np.array(srer.noise_stds)
+            zs = np.array(srer.probed_replay_probs)
+            mask = zs >= 0
+
+            axs[-1].scatter(xs[mask], ys[mask], c=zs[mask], vmin=0, vmax=max_prob, lw=0)
+            axs[-1].scatter(xs[~mask], ys[~mask], c='gray', marker='x')
+            axs[-1].set_title('g_x = {0:.2f}'.format(g_x))
+
+    for ax in axs:
+        ax.set_xlim(X_LIM)
+        ax.set_ylim(Y_LIM)
+        ax.set_xlabel('g_w')
+
+        ax.set_xticks([ax.get_xticks()[0], ax.get_xticks()[-1]])
+
+    axs[0].set_ylabel('noise std')
+
+    # get parameters to use for example from last database entry
+    d = srer.network_size
+    v_th = srer.v_th
+    rp = srer.rp
+    t_x = srer.t_x
+    node_seq = [tuple(pair) for pair in srer.sequence]
+    drive_amp = srer.drive_amplitude
+    probe_time = srer.probe_time
+    alpha = srer.alpha
+
+    # run examples
+    w_base, nodes = connectivity.hexagonal_lattice(d)
 
     r_0 = np.zeros((len(nodes),))
     xc_0 = np.zeros((len(nodes),))
 
-    # set up figure layout
-    gs = gridspec.GridSpec(len(SEEDS_EXAMPLE), 3)
-    fig = plt.figure(figsize=(15, 2 * len(SEEDS_EXAMPLE)), tight_layout=True)
-
     # run examples
-    axs_example = [fig.add_subplot(gs[0, :2])]
+    axs_example = [fig.add_subplot(gs[1, :])]
     axs_example.extend([
-        fig.add_subplot(gs[1+ctr, :2], sharex=axs_example[0], sharey=axs_example[0])
+        fig.add_subplot(gs[2+ctr, :], sharex=axs_example[0], sharey=axs_example[0])
         for ctr in range(len(SEEDS_EXAMPLE) - 1)])
 
     # make network
     ntwk = network.LocalWtaWithAthAndStdp(
-        th=TH, w=W*w_base, g_x=G_X, t_x=T_X, rp=RP,
-        stdp_params=None, wta_dist=2, wta_factor=0)
+        th=v_th, w=W*w_base, g_x=G_X, t_x=t_x, rp=rp,
+        stdp_params=None, wta_dist=2, wta_factor=alpha)
 
     # make base drives for examples (which we'll add noise to)
-    n_steps = REPLAY_TRIGGER + len(NODE_SEQ) + 2
+    n_steps = probe_time + len(node_seq) + 2
     drives_base = np.zeros((n_steps, len(nodes)))
 
     # initial sequence
-    for ctr, node in enumerate(NODE_SEQ):
+    for ctr, node in enumerate(node_seq):
         node_idx = nodes.index(node)
-        drives_base[ctr + 1, node_idx] = DRIVE_AMP
+        drives_base[ctr + 1, node_idx] = drive_amp
 
     # replay trigger
-    drives_base[REPLAY_TRIGGER, nodes.index(NODE_SEQ[0])] = DRIVE_AMP
+    drives_base[probe_time, nodes.index(node_seq[0])] = drive_amp
 
     # run network
     for seed, noise, ax in zip(SEEDS_EXAMPLE, NOISES_EXAMPLE, axs_example):
@@ -179,51 +234,13 @@ def extension_by_spontaneous_replay(
         r_times, r_nodes = rs.nonzero()
 
         ax.scatter(r_times, r_nodes, s=15, lw=0)
-        ax.set_xlim(-REPLAY_TRIGGER*0.05, REPLAY_TRIGGER*1.05)
+        ax.set_xlim(-probe_time*0.05, probe_time*1.05)
         ax.set_ylabel('node')
         ax.set_title('noise stdev = {0:.3}'.format(noise))
 
     axs_example[-1].set_xlabel('time step')
 
-    # run statistics
-    ax_stats = fig.add_subplot(gs[:, -1])
-    colors = plot.get_n_colors(len(WS_STATS), 'jet')
-    handles = []
+    for ax in axs + axs_example: plot.set_fontsize(ax, 16)
 
-    for w, g_x, color in zip(WS_STATS, G_XS_STATS, colors):
-
-        label = 'w = {0:.1f}, g_x = {1:.1f}'.format(w, g_x)
-        ntwk = network.LocalWtaWithAthAndStdp(
-            th=TH, w=w*w_base, g_x=g_x, t_x=T_X, rp=RP,
-            stdp_params=None, wta_dist=2, wta_factor=0)
-
-        np.random.seed(SEED_STATS)
-        replay_probs = []
-
-        for noise in NOISES_STATS:
-            correct_replays = 0
-
-            for _ in range(N_TRIALS):
-
-                # create new instance of noisy drives and run network
-                drives = drives_base + noise * np.random.randn(*drives_base.shape)
-                rs, _ = ntwk.run(r_0=r_0, xc_0=xc_0, drives=drives)
-
-                # compare triggered sequence to initial sequence
-                rs_initial = rs[1:1+len(NODE_SEQ)]
-                rs_replay = rs[REPLAY_TRIGGER:REPLAY_TRIGGER+len(NODE_SEQ)]
-
-                if np.all(rs_initial == rs_replay): correct_replays += 1
-
-            replay_probs.append(correct_replays / N_TRIALS)
-
-        handles.append(ax_stats.plot(replay_probs, NOISES_STATS, lw=2, label=label)[0])
-
-    ax_stats.invert_yaxis()
-    ax_stats.set_ylabel('noise level')
-    ax_stats.set_xlabel('correct replay probability\n(at t = {})'.format(REPLAY_TRIGGER))
-    ax_stats.legend(handles=handles, loc='best')
-
-    for ax in axs_example + [ax_stats]: plot.set_fontsize(ax, 16)
-
+    session.close()
     return fig
