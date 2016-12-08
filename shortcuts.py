1cd540eb805565cb7c1a766c2c98e71a9e576cc5
rkp8000 <rpang.contact@gmail.com>
2016-12-08 14:10:48 -0600

    Fix reorder_indexes function to actually work.

--- shortcuts.py
+++ shortcuts.py
@@ -1,5 +1,41 @@
 from __future__ import division, print_function
 from itertools import product as cproduct
+import numpy as np
+
+
+def make_drive_seq(seq, nodes, start, shape):
+    """
+    Convert a sequence of nodes to a logical array.
+    :param seq:
+    :param nodes:
+    :param start:
+    :param shape:
+    :return:
+    """
+    drives = np.zeros(shape)
+
+    for ctr, node in enumerate(seq):
+        drives[start + ctr, nodes.index(node)] = 1
+
+    return drives
+
+
+def reorder_idxs(original, first):
+    """
+    Reorder an array so that a specific ordered set of elements appear first.
+    Return the reordered array and the indexes that were used to do the reordering.
+    """
+    if len(set(original)) != len(original):
+        raise Exception('original array must not contain repeated element')
+    if len(set(first)) != len(first):
+        raise Exception('first elements must not contain repeated element')
+    if np.any([x not in original for x in first]):
+        raise Exception('first elements contain unknown element')
+
+    reordered = list(first) + [x for x in original if x not in first]
+    idxs_reordered = [original.index(x) for x in reordered]
+
+    return reordered, idxs_reordered
 
 
 def zip_cproduct(z, c, order, kwargs):
